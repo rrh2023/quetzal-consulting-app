75da286ee0b02c5132cc44e11a37328252ef9d29
Roberto Rodas-Herndon <rrh2023@bu.edu>
2025-10-07 08:30:35 -0400

    Create routes: users.js, locations.js, auth.js

---
+++ server/routes/locations.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+// @route     GET api/locations
+// @desc      Get all users contacts
+// @access    Private
+router.get("/", (req, res) => {
+  res.send("Get all locations");
+});
+
+// @route     GET api/locations
+// @desc      Add new location
+// @access    Private
+router.post("/", (req, res) => {
+  res.send("Add contact");
+});
+
+// @route     PUT api/locations/:id
+// @desc      Update contact
+// @access    Private
+router.put("/:id", (req, res) => {
+  res.send("Get all locations");
+});
+
+// @route     DELETE api/locations/:id
+// @desc      Delete contact
+// @access    Private
+router.delete("/:id", (req, res) => {
+  res.send("Delete location");
+});
+
+module.exports = router;
